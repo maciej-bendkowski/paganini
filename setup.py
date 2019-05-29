893bcfd12325b22613ee2202aa0fc6b529916b94
Maciej Bendkowski <maciej.bendkowski@gmail.com>
2019-05-29 16:42:29 +0200

    NEW: Paganini version 1.0

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name = "paganini",
+    version = "1.0.0",
+
+    author = "Maciej Bendkowski, Sergey Dovgal",
+    author_email = "maciej.bendkowski@tcs.uj.edu.pl, vic.north@gmail.com",
+    description = "Multiparametric tuner for combinatorial specifications",
+
+    license = "BSD3",
+    url = "https://github.com/maciej-bendkowski/paganini",
+    install_requires = ['numpy', 'sympy', 'cvxpy'],
+    packages = find_packages(),
+
+    # unit tests
+    test_suite = "tests.singular_problems"
+)
