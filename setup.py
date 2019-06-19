2a1939cabcaba656a0bc8ef1d9b5842fa1f7879f
Electric-tric <vit.north@gmail.com>
2019-06-19 17:51:17 +0200

    Bump version v1.1.2

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup, find_packages
 
 setup(
     name = "paganini",
-    version = "1.1.1",
+    version = "1.1.2",
 
     author = "Maciej Bendkowski, Sergey Dovgal",
-    author_email = "maciej.bendkowski@tcs.uj.edu.pl, vic.north@gmail.com",
+    author_email = "maciej.bendkowski@tcs.uj.edu.pl, vit.north@gmail.com",
     description = "Multiparametric tuner for combinatorial specifications",
 
     license = "BSD3",
     url = "https://github.com/maciej-bendkowski/paganini",
-    install_requires = ['numpy', 'sympy', 'cvxpy'],
+    install_requires = ['numpy', 'sympy', 'cvxpy', 'scipy'],
     packages = find_packages(),
 
     python_requires='>=3.0', # drop support Python 2
