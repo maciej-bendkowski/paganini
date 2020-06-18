7eb876e96621adf4cfb103fb5bfbe83a201053a1
Maciej Bendkowski <maciej.bendkowski@gmail.com>
2020-06-18 16:29:19 +0200

    UPT: Bump version 1.3.3.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "paganini",
-    version = "1.3.2",
+    version = "1.3.3",
 
     author = "Maciej Bendkowski, Sergey Dovgal",
     author_email = "maciej.bendkowski@tcs.uj.edu.pl, vit.north@gmail.com",
